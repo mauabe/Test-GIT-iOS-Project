02d98e6b2813d23d4642b5cc8e9f5707e35ea2b8
Mauricio <maumau@MauBookPro17.local>
2014-03-09 17:04:18 -0700

    added a test class

---
+++ Git Test/SDTest.h
@@ -0,0 +1,13 @@
+//
+//  SDTest.h
+//  Git Test
+//
+//  Created by Mauricio on 3/9/14.
+//  Copyright (c) 2014 Spepy Design Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SDTest : NSObject
+
+@end
---
+++ Git Test/SDTest.m
@@ -0,0 +1,13 @@
+//
+//  SDTest.m
+//  Git Test
+//
+//  Created by Mauricio on 3/9/14.
+//  Copyright (c) 2014 Spepy Design Inc. All rights reserved.
+//
+
+#import "SDTest.h"
+
+@implementation SDTest
+
+@end
